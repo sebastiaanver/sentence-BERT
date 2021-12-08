2cd8fefb4ccb76501e181f5e44d44dbf9268bd58
annasanchez27 <annasanchezespunyes27@gmail.com>
2021-12-08 21:23:20 +0100

    added spearmanr
    Co-Authored-By: sebastiaanver <37439110+sebastiaanver@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 import numpy as np
 
+from scipy import stats
 from dataset import load_data
 from model import SentenceBert
 from transformers import BertTokenizer
@@ -43,11 +44,17 @@ def main():
             optimizer.step()
 
     predictions = np.array([])
+    labels = np.array([])
     for local_batch, local_labels in test_generator:
         sent_a, sent_b = local_batch["sent_a"], local_batch["sent_b"]
         y_pred = model(sent_a, sent_b)
         predictions = np.append(predictions, y_pred.numpy)
+        labels = np.append(labels, local_labels.numpy)
     np.save("predictions.npy", predictions)
+    np.save("labels.npy", labels)
+
+    r = stats.spearmanr(predictions, labels)
+    np.save("results.npy", np.array([r.correlation]))
 
 
 if __name__ == "__main__":
