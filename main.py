5a3f76e546c8ca772eb120f3a5330749a3acb869
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-08 23:18:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import torch
+import tqdm
+import numpy as np
 import argparse
 
+from scipy import stats
 from dataset import load_data
 from model import SentenceBert
 from transformers import BertTokenizer
@@ -42,6 +45,18 @@ def main():
             optimizer.step()
 
     torch.save(model.state_dict(), "model")
+
+    with torch.no_grad():
+        predictions = np.array([])
+        labels = np.array([])
+        for local_batch, local_labels in tqdm.tqdm(test_generator):
+            sent_a, sent_b = local_batch["sent_a"], local_batch["sent_b"]
+            y_pred = model(sent_a, sent_b)
+            predictions = np.append(predictions, y_pred.cpu().detach().numpy())
+            labels = np.append(labels, local_labels.cpu().detach().numpy())
+
+        r = stats.spearmanr(predictions, labels)
+        print(f"Spearman correlation: {r.correlation}")
 
 
 if __name__ == "__main__":
