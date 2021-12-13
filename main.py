6437a6a730cbcd8fea9ade1f92f318b58578dfeb
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-13 22:26:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main():
             optimizer.step()
             scheduler.step()
 
-            if step % 100 == 0:
+            if step % 300 == 0:
                 print(f"Current lr: {scheduler.get_last_lr()}")
                 with torch.no_grad():
                     predictions = np.array([])
