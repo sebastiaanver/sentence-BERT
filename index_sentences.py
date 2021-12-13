46050b9fc71428fa3da3e5ea3b94f1a044b396b4
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-13 14:26:37 +0100

    Update streamlit

---
+++ index_sentences.py
@@ -0,0 +1,7 @@
+
+def index_sentences():
+    pass
+
+
+if __name__ == "__main__":
+    index_sentences()
--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,21 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+from scipy import spatial
+
+from transformers import BertModel, BertTokenizer
+from model import SentenceBertInference
+
+
+# Read numpy files for search functionality
+sentences = np.load("data/sentences.npy", allow_pickle=True)
+vectors = np.load("data/vectors.npy", allow_pickle=True)
+tree = spatial.KDTree(data=vectors)
+
+# Init model
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert_model = BertModel.from_pretrained('sebastiaan/sentence-BERT-regression')
+model = SentenceBertInference(tokenizer, bert_model)
 
 # Results from experiments
 experiments = ["Classification task", "Regression task", "Combined task"]
@@ -18,10 +34,12 @@ st.table(df)
 st.title('Search engine')
 model_to_use = st.selectbox('Select model to use:', experiments)
 query = st.text_input('Search query')
+
+query_vec = model.predict(query).cpu().detach().numpy()
+res = tree.query(query_vec, k=5)
+
 st.text(f"Results for model: '{model_to_use}' and query: '{query}'")
 
-for i in range(5):
-    st.text("This is a sentence result.")
-
-
+for i in res[1]:
+    st.text(sentences[i])
 
