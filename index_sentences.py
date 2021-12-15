e28b675790d9b280d2d2d030acfbe6a163f2b3f6
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-15 19:13:44 +0100

    Indexed files for all models

--- index_sentences.py
+++ index_sentences.py
@@ -1,6 +1,39 @@
+from datasets import load_dataset
+from model import SentenceBertInference
+from transformers import BertModel, BertTokenizer
+
+import torch
+import argparse
+import numpy as np
+
 
 def index_sentences():
-    pass
+    parser = argparse.ArgumentParser(description="Index sentences")
+    parser.add_argument("--model", type=str, default="sentence-BERT-combined")
+    args = parser.parse_args()
+
+    model_path = "combined"
+    if args.model == "sentence-BERT-regression":
+        model_path = "regression"
+    elif args.model == "sentence-BERT-classifcation":
+        model_path = "classification"
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    bert_model = BertModel.from_pretrained(f"sebastiaan/{args.model}")
+
+    model = SentenceBertInference(tokenizer, bert_model)
+
+    dataset = load_dataset("Abirate/english_quotes")
+    df = dataset["train"].to_pandas()
+    df["quote"].apply(lambda x: x.split(".")[0] + '."')
+
+    df["vec"] = df["quote"].apply(lambda x: model.predict(x).cpu().detach().numpy()[0])
+
+    sentences = df["quote"].values
+    vectors = np.vstack(np.ravel(np.array(df["vec"].values)))
+
+    np.save(f"{model_path}/sentences.npy", sentences)
+    np.save(f"{model_path}/vectors.npy", vectors)
 
 
 if __name__ == "__main__":
